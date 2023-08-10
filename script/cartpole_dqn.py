ad6c699119fcb9a2eb398e7cd0205410a6c45d45
KrissyDong <chrisdong@berkeley.edu>
2023-08-09 18:58:10 -0700

    rl_script_for_cartpool

---
+++ script/cartpole_dqn.py
@@ -0,0 +1,138 @@
+## This is course material for Introduction to Modern Artificial Intelligence
+## Example code: cartpole_dqn.py
+## Author: Allen Y. Yang
+##
+## (c) Copyright 2020. Intelligent Racing Inc. Not permitted for commercial use
+
+# Please make sure to install openAI gym module
+# https://github.com/openai/gym
+import random
+# from sre_parse import FLAGS
+import gym
+import os
+import numpy as np
+from collections import deque
+from keras.models import Sequential
+from keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+
+from absl import app
+from absl import FLAGS
+from absl import logging
+import tensorflow as tf
+
+
+EPISODES = 100
+
+class DQNAgent:
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=10000)
+        self.gamma = 0.95    # discount rate
+        self.epsilon = 1.0  # exploration rate
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.learning_rate = 0.001
+        self.model = self._build_model()
+
+    def _build_model(self):
+        # Neural Net for Deep-Q learning Model
+        model = Sequential()
+        model.add(Dense(12, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(12, activation='relu'))
+        model.add(Dense(self.action_size, activation='linear'))
+        model.compile(loss='mse',
+                      optimizer=Adam(learning_rate=self.learning_rate))
+        return model
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        act_values = self.model.predict(state, verbose=0)
+        return np.argmax(act_values[0])  # returns action
+
+    def replay(self, batch_size):
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = reward
+            if not done:
+                target = (reward + self.gamma *
+                          np.amax(self.model.predict(next_state, verbose=0)[0]))
+            target_f = self.model.predict(state, verbose=0)
+            target_f[0][action] = target
+            self.model.fit(state, target_f, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def load(self, name):
+        self.model.load_weights(name)
+
+    def save(self, name):
+        # self.model.save_weights(name)
+
+        # Convert and save the model to .tflite
+        tflite_model = convert_tflite_model(self.model)
+        save_tflite_model(tflite_model,
+                            FLAGS.save_dir,
+                            model_name="hello_world_float.tflite")
+
+def convert_tflite_model(model):
+  """Convert the save TF model to tflite model, then save it as .tflite flatbuffer format
+    Args:
+        model (tf.keras.Model): the trained hello_world Model
+    Returns:
+        The converted model in serialized format.
+  """
+  converter = tf.lite.TFLiteConverter.from_keras_model(model)
+  tflite_model = converter.convert()
+  return tflite_model
+
+
+def save_tflite_model(tflite_model, save_dir, model_name):
+  """save the converted tflite model
+  Args:
+      tflite_model (binary): the converted model in serialized format.
+      save_dir (str): the save directory
+      model_name (str): model name to be saved
+  """
+  if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+  save_path = os.path.join(save_dir, model_name)
+  with open(save_path, "wb") as f:
+    f.write(tflite_model)
+  logging.info("Tflite model saved to %s", save_dir)
+
+
+if __name__ == "__main__":
+    env = gym.make('CartPole-v1')
+    state_size = env.observation_space.shape[0]
+    action_size = env.action_space.n
+    agent = DQNAgent(state_size, action_size)
+    # agent.load("./save/cartpole-dqn.h5")
+    done = False
+    batch_size = 32
+
+    for e in range(EPISODES):
+        state = env.reset()
+        state = np.reshape(state, [1, state_size])
+        for time in range(500):
+            # env.render()
+            action = agent.act(state)
+            next_state, reward, done, _ = env.step(action)
+            env.render()
+            reward = reward if not done else -10
+            next_state = np.reshape(next_state, [1, state_size])
+            agent.remember(state, action, reward, next_state, done)
+            state = next_state
+            if done:
+                print("episode: {}/{}, score: {}, e: {:.2}"
+                      .format(e, EPISODES, time, agent.epsilon))
+                break
+            if len(agent.memory) > batch_size:
+                agent.replay(batch_size)
+        # if e % 10 == 0:
+        #     agent.save("./save/cartpole-dqn.h5")
